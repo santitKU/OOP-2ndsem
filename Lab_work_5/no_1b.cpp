f474cc37263ad6fe6c9bb736c20d094ce504005f
santitKU <ss26121321@student.ku,edu.np>
2022-12-07 14:59:45 +0545

    labwork 5 number 1 b completed

---
+++ Lab_work_5/no_1b.cpp
@@ -0,0 +1,50 @@
+/*Multilevel inheritance*/
+
+#include<iostream>
+
+using namespace std;
+
+class Student{
+    protected:
+        int roll_number;
+    public:
+        void set_roll_number(int r){
+            roll_number = r;
+        }
+        void get_roll_number(){
+            cout<<"The roll number is "<<roll_number<<endl;
+        }
+};
+
+class Exam: public Student{
+    protected:
+        float maths;
+        float physics;
+    public:
+        void set_marks(float m1, float m2){
+            maths = m1;
+            physics = m2;
+        }
+        void get_marks(){
+            cout<<"The marks obtained in maths are: "<<maths<<endl;
+            cout<<"The marks obtained in physics are: "<<physics<<endl;
+        }
+};
+
+class Result:public Exam{
+    float percentage;
+    public:
+        void display_results(){
+            get_roll_number();
+            get_marks();
+            cout<<"Your result is "<<(maths+physics)/2<<" %"<<endl;
+        }
+};
+
+int main(){
+    Result santit;
+    santit.set_roll_number(420);
+    santit.set_marks(94.0, 90.0);
+    santit.display_results();
+    return 0;
+}
